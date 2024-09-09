65c8c23215433aae3125d29970150fcaa1aaa334
Feras <99082748@mydavinci.nl>
2024-09-09 19:37:31 +0200

    Create demo1.py

---
+++ les4/demo1.py
@@ -0,0 +1,16 @@
+var_a = 3
+var_b = 7
+
+getal= int(input('geef getal a:'))
+if getal < var_b:
+    print('a > b')
+getal= int(input('geef getal b:'))
+if getal > var_a:
+    print('b > a')
+geetal= int(input('geef een getal:'))
+if getal < var_b:
+    print('a en b zijn gelijk')
+if getal > var_a:
+    print('b en a zijn gelijk')
+else:
+    print('onbekend')        
