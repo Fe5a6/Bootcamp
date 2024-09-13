4a1c639d2a2ec972e0f8baa311dfefca0e6f6b86
Feras <99082748@mydavinci.nl>
2024-09-13 14:00:35 +0200

    Create demo2.py

---
+++ les7/demo2.py
@@ -0,0 +1,10 @@
+leeftijd = 18
+snor     = 'j'
+dilpoma  = 'j'
+
+
+# if statment 1
+if (leeftijd >= 18 and snor == 'j') or (leeftijd < 18 and diploma == 'j'):
+    print("Je bent aangenomen!")
+else:
+    print("Je bent niet aangenomen.")
