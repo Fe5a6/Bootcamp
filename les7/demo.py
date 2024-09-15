257f864063fd68ceb6245a72d6383149d3a947d3
Feras <99082748@mydavinci.nl>
2024-09-15 15:39:47 +0200

    Create demo.py

---
+++ les7/demo.py
@@ -0,0 +1,7 @@
+leeftijd = int(input('voer je leeftijd in:'))
+
+if not (leeftijd > 18):
+    print('Geen toegang, je bent onder de 18')
+
+else:
+    print('Toegang toegestaa, Veel plezier bij het evenement')
